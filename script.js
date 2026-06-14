177c93957f6437bef8f30888535e6fc461f5027f
ViniSan25 <135791084+ViniSan25@users.noreply.github.com>
2026-06-14 03:01:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Scroll suave dos links
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
     anchor.addEventListener("click", function(e) {
@@ -11,4 +13,29 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
     });
 
+});
+
+
+// Animação dos serviços
+
+const cards = document.querySelectorAll(".servico-card");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+    threshold: 0.25
+});
+
+cards.forEach(card => {
+    observer.observe(card);
 });
